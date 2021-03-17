0e4734a84840c91c6610aa1f75112369bfa2b8d4
Andrea <andrea.ae@live.it>
2021-03-17 14:27:58 +0100

    aggiunto azztrasp che sposta il flag trasporti da f a c ed azzera f

---
+++ Source/azztrasp.cbl
@@ -0,0 +1,36 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.                      azztrasp.
+       AUTHOR.                          Andrea.
+       REMARKS. In tutti i listini fornitore sposta il valore del flag
+           fornitori sui clienti ed azzera il fornitore
+  
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           copy "tlistini.sl".
+
+      *****************************************************************
+       DATA DIVISION.
+       FILE SECTION.
+           copy "tlistini.fd".
+
+       WORKING-STORAGE SECTION.
+       77  status-tlistini  pic xx.
+
+       PROCEDURE DIVISION.
+
+      ***---
+       MAIN.                                  
+           open i-o tlistini.
+
+           move low-value to tlis-rec.
+           start tlistini key >= tlis-chiave.
+           perform until 1 = 2
+              read tlistini next no lock at end exit perform end-read
+              move tlis-trasp-f to tlis-trasp-c
+              move 0 to tlis-trasp-f
+              rewrite tlis-rec
+           end-perform.
+
+           close tlistini.
+
+           goback.
