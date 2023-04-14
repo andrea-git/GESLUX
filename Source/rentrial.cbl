fdb907d551500ec765f6a3c4aaae699748bac4b6
Andrea <andrea.ae@live.it>
2023-04-14 17:39:11 +0200

    ricmpilati programmi blocco ordfor2

--- Source/rentrial.cbl
+++ Source/rentrial.cbl
@@ -54,66 +54,8 @@
                  exit perform cycle
               end-if
               add 1 to nFiles
-      *****        move "Heavy-Gain-"  to nome-file2
-      *****        move nome-file(7:)  to nome-file2(12:)
-      *****        initialize cmd                                          
-      *****        inspect nome-file  replacing trailing spaces by low-value
-      *****        inspect nome-file2 replacing trailing spaces by low-value
-      *****        string "move "    delimited size
-      *****              x"22"       delimited size
-      *****               dir-m      delimited size
-      *****               nome-file  delimited low-value
-      *****              x"22"       delimited size
-      *****               " "        delimited size                  
-      *****              x"22"       delimited size
-      *****               dir-m      delimited size
-      *****               nome-file2 delimited low-value
-      *****              x"22"       delimited size
-      *****          into cmd
-      *****        end-string
-      *****        call "C$SYSTEM" using cmd, 225
-           end-perform.
-
-           call "C$LIST-DIRECTORY" using LISTDIR-CLOSE, dir-handle.
-           call "C$LIST-DIRECTORY" using LISTDIR-OPEN,
-                                   dir-m
-                                   "*.mp3"
-
-           move return-code to dir-handle.
-
-           if dir-handle = 0
-              goback
-           end-if
-
-           perform until 1 = 2
-              call "C$LIST-DIRECTORY" using LISTDIR-NEXT,
-                                            dir-handle,
-                                            nome-file
-
-              if nome-file = spaces exit perform end-if
-
-              if nome-file = "."      or
-                           = ".."     or
-                           = "Backup" or
-                           = ".DS_Store"
-                 exit perform cycle
-              end-if
-
-              perform until 1 = 2
-                 compute n = function random * (nFiles)
-                 if n > nFiles stop "L" end-if
-                 add 1 to n
-                 if el-n(n) not = "X"
-                    if n = 2 or 4 or 6 or 8 stop n end-if
-                    move "X" to el-n(n)
-                    exit perform
-                 end-if
-              end-perform
-              move n         to nome-file2
-              move " - "     to nome-file2(5:)
+              move "Heavy - "  to nome-file2
               move nome-file to nome-file2(9:)
-      *****        move "Heavy-Gain-"  to nome-file2
-      *****        move nome-file(7:)  to nome-file2(12:)
               initialize cmd                                          
               inspect nome-file  replacing trailing spaces by low-value
               inspect nome-file2 replacing trailing spaces by low-value
@@ -133,5 +75,63 @@
            end-perform.
 
            call "C$LIST-DIRECTORY" using LISTDIR-CLOSE, dir-handle.
+      *****     call "C$LIST-DIRECTORY" using LISTDIR-OPEN,
+      *****                             dir-m
+      *****                             "*.mp3"
+      *****
+      *****     move return-code to dir-handle.
+      *****
+      *****     if dir-handle = 0
+      *****        goback
+      *****     end-if
+      *****
+      *****     perform until 1 = 2
+      *****        call "C$LIST-DIRECTORY" using LISTDIR-NEXT,
+      *****                                      dir-handle,
+      *****                                      nome-file
+      *****
+      *****        if nome-file = spaces exit perform end-if
+      *****
+      *****        if nome-file = "."      or
+      *****                     = ".."     or
+      *****                     = "Backup" or
+      *****                     = ".DS_Store"
+      *****           exit perform cycle
+      *****        end-if
+      *****
+      *****        perform until 1 = 2
+      *****           compute n = function random * (nFiles)
+      *****           if n > nFiles stop "L" end-if
+      *****           add 1 to n
+      *****           if el-n(n) not = "X"
+      *****              if n = 2 or 4 or 6 or 8 stop n end-if
+      *****              move "X" to el-n(n)
+      *****              exit perform
+      *****           end-if
+      *****        end-perform
+      *****        move n         to nome-file2
+      *****        move " - "     to nome-file2(5:)
+      *****        move nome-file to nome-file2(9:)
+      **********        move "Heavy-Gain-"  to nome-file2
+      **********        move nome-file(7:)  to nome-file2(12:)
+      *****        initialize cmd                                          
+      *****        inspect nome-file  replacing trailing spaces by low-value
+      *****        inspect nome-file2 replacing trailing spaces by low-value
+      *****        string "move "    delimited size
+      *****              x"22"       delimited size
+      *****               dir-m      delimited size
+      *****               nome-file  delimited low-value
+      *****              x"22"       delimited size
+      *****               " "        delimited size                  
+      *****              x"22"       delimited size
+      *****               dir-m      delimited size
+      *****               nome-file2 delimited low-value
+      *****              x"22"       delimited size
+      *****          into cmd
+      *****        end-string
+      *****        call "C$SYSTEM" using cmd, 225
+      *****     end-perform.
+
+      *****     call "C$LIST-DIRECTORY" using LISTDIR-CLOSE, dir-handle.
 
            goback.
