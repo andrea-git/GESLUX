08a97e6ea2b229ca0bc50f73d95f6c9947cf9f75
Andrea Eventi <andrea.ae@live.it>
2024-02-21 11:11:27 +0100

    tolta opzioni di compilazione xfd

--- XFD4.bat
+++ XFD4.bat
@@ -1,3 +1,3 @@
 SET COPYPATH=F:\Lubex\GESLUX\fd;F:\Lubex\GESLUX\LAB\fd;F:\Lubex\GESLUX\copylib;C:\Acucorp\Acucbl610\AcuGT\sample\def;F:\Lubex\GESLUX\EDI\Copylib;F:\Lubex\GESLUX\EDI\FD;F:\Lubex\GESLUX\SHI\fd;
-"C:\Program Files (x86)\Micro Focus\Acucbl910\AcuGT\bin\ccbl32.exe" -o F:\Lubex\GESLUX\Object\genfiles.acu -x -CK -Fx4 -Fo F:\Lubex\GESLUX\xfd F:\Lubex\GESLUX\Source\genfiles.cbl
+"C:\Program Files (x86)\Micro Focus\Acucbl910\AcuGT\bin\ccbl32.exe" -o F:\Lubex\GESLUX\Object\genfiles.acu -x -Fx4 -Fo F:\Lubex\GESLUX\xfd F:\Lubex\GESLUX\Source\genfiles.cbl
 pause 0
